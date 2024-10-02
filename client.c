eddd409ba135f1f81d5864a82797bda3988a2c8c
WilliamLebrato <william.lebrato@gmail.com>
2024-10-02 14:47:56 +0200

    Sending / Receiving First message

--- client.c
+++ client.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <errno.h>
+
 
 #define BUFFER_SIZE 1024
 
@@ -25,7 +27,7 @@ int main(int argc, char *argv[]){
 
     if (!Destport) {
         fprintf(stderr, "Error: Invalid format. Use <server_address:port>.\n");
-        exit(EXIT_FAILURE);
+        return -1;
     }
 
     char *nickname = argv[2];
@@ -33,10 +35,70 @@ int main(int argc, char *argv[]){
     // Validate the nickname (12 characters max, only A-Za-z0-9_)
     if (strlen(nickname) > 12 || strspn(nickname, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_") != strlen(nickname)) {
         fprintf(stderr, "Invalid nickname. Must be up to 12 characters long and can contain A-Z, a-z, 0-9, and _.\n");
-        exit(EXIT_FAILURE);
+        return -1;
     }
 
+    // Set up the socket
+    int sockfd;
+    int rv;
+    char buf[BUFFER_SIZE];
+    struct addrinfo hints, *servinfo, *p;
 
-  return 0;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;     // Allow IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+
+
+    if ((rv = getaddrinfo(Desthost, Destport, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return -1;
+    }
+
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("socket");
+            continue;
+        }
+
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("connect");
+            continue;
+        }
+
+        break;  // Successfully connected
+    }
+
+    freeaddrinfo(servinfo);
+
+    char message[20];
+    snprintf(message, sizeof(message), "NICK %s\n", nickname);
+
+    if (send(sockfd, message, strlen(message), 0) == -1) {
+        perror("send");
+        close(sockfd);
+        return -1;
+    }
+
+    memset(buf, 0, sizeof buf);
+    if (recv(sockfd, buf, BUFFER_SIZE - 1, 0) == -1) {
+        perror("recv");
+        close(sockfd);
+        return -1;
+    }
+
+    // Check if the server accepted the nickname
+    if (strstr(buf, "OK") == NULL) {
+        printf("ERROR: Nickname not accepted by the server\n");
+        close(sockfd);
+        return -1;
+    }
+    
+
+
+
+
+    return 0;
+
 
 }
