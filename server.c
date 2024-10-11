1d54aba6211bde15d6872280df3f2e46f3df1c81
WilliamLebrato <william.lebrato@gmail.com>
2024-10-11 17:34:42 +0200

    Add Client, Remove Client & Broadcast

--- server.c
+++ server.c
@@ -7,6 +7,69 @@
 #include <unistd.h>
 #include <errno.h>
 
+struct Client {
+    int sockfd;
+    char ip_address[INET6_ADDRSTRLEN];
+    char nickname[13];
+    int is_active;
+};
+
+struct Client clients[SOMAXCONN];
+
+void initialize_clients() {
+    for (int i = 0; i < SOMAXCONN; i++) {
+        clients[i].is_active = 0;
+        clients[i].sockfd = -1;
+    }
+}
+
+int add_client(int client_sockfd, struct sockaddr_storage *client_addr) {
+    for (int i = 0; i < SOMAXCONN; i++) {
+        if (clients[i].is_active == 0) {
+            clients[i].sockfd = client_sockfd;
+            clients[i].is_active = 1;
+
+            if (client_addr->ss_family == AF_INET) {
+                struct sockaddr_in *addr = (struct sockaddr_in *)client_addr;
+                inet_ntop(AF_INET, &(addr->sin_addr), clients[i].ip_address, sizeof(clients[i].ip_address));
+            } else if (client_addr->ss_family == AF_INET6) {
+                struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *)client_addr;
+                inet_ntop(AF_INET6, &(addr6->sin6_addr), clients[i].ip_address, sizeof(clients[i].ip_address));
+            }
+
+            memset(clients[i].nickname, 0, sizeof(clients[i].nickname));
+
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void remove_client(int client_sockfd) {
+    for (int i = 0; i < SOMAXCONN; i++) {
+        if (clients[i].sockfd == client_sockfd && clients[i].is_active) {
+            clients[i].is_active = 0;
+            close(clients[i].sockfd);
+            clients[i].sockfd = -1;
+            printf("Client %s disconnected.\n", clients[i].ip_address);
+            break;
+        }
+    }
+}
+
+void broadcast(int sender_sockfd, const char *message) {
+    for (int i = 0; i < SOMAXCONN; i++) {
+        if (clients[i].is_active && clients[i].sockfd != sender_sockfd) {
+            if (send(clients[i].sockfd, message, strlen(message), 0) == -1) {
+                perror("send failed");
+                remove_client(clients[i].sockfd);
+            }
+        }
+    }
+}
+
+
+
 int main(int argc, char *argv[]) {
     // Check if the user provided the required command-line argument
     if (argc != 2) {
@@ -27,7 +90,7 @@ int main(int argc, char *argv[]) {
 
     // Convert port string to integer
     int port = atoi(Destport);
-    if (port <= 0 || port > 65535) {
+    if (port <= 0) {
         fprintf(stderr, "Error: Invalid port number.\n");
         return EXIT_FAILURE;
     }
@@ -70,7 +133,19 @@ int main(int argc, char *argv[]) {
 
     freeaddrinfo(res); // Free the linked list after binding
 
+    if (listen(sockfd, SOMAXCONN) == -1) {
+        perror("listen() failed");
+        close(sockfd);
+        return EXIT_FAILURE;
+    }
     
+
+
+    while(1)
+    {
+
+    }
+
     close(sockfd);
     return EXIT_SUCCESS;
 }
